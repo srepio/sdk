9ef690924cdaabbf6963555e46f9826de3f1f98b
Henry Whitaker <henrywhitaker3@outlook.com>
2023-09-09 21:30:12 +0100

    added create user request struct

---
+++ users/requests.go
@@ -0,0 +1,7 @@
+package users
+
+type CreateUserRequest struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
