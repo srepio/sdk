bb91a52fe9dbbe3de7145f95f2b8bc81a6a809b0
Henry Whitaker <henrywhitaker3@outlook.com>
2023-09-10 10:07:32 +0100

    added login types

--- client/users.go
+++ client/users.go
@@ -17,6 +17,20 @@ type CreateUserResponse struct {
 	User *types.User
 }
 
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type LoginResponse struct {
+	User  *types.User
+	Token string
+}
+
 func (c *Client) CreateUser(ctx context.Context, req *CreateUserRequest) (*CreateUserResponse, error) {
+	return nil, errors.New("not implemented yet")
+}
+
+func (c *Client) Login(ctx context.Context, req *LoginRequest) (*LoginResponse, error) {
 	return nil, errors.New("not implemented yet")
 }
