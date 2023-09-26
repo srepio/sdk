ae68dbd7458c3b46448b1c95ae8fd72858b6efa9
Henry Whitaker <henrywhitaker3@outlook.com>
2023-09-26 23:12:08 +0100

    move port/vol structs to types

--- types/docker/container.go
+++ types/docker/container.go
@@ -1,10 +1,12 @@
 package docker
 
+import "github.com/srepio/sdk/types"
+
 type Container struct {
 	Id         string
 	Name       string
 	Image      string
-	Ports      []Port
-	Volumes    []Volume
+	Ports      []types.Port
+	Volumes    []types.Volume
 	Privileged bool
 }
--- types/metadata.go
+++ types/metadata.go
@@ -1,16 +1,24 @@
 package types
 
-import "github.com/srepio/sdk/types/docker"
-
 type Metadata []Scenario
 
+type Port struct {
+	Host      string `json:"host"`
+	Container string `json:"container"`
+}
+
+type Volume struct {
+	Host      string `json:"host"`
+	Container string `json:"container"`
+}
+
 type Scenario struct {
-	Name        string          `json:"name"`
-	Description string          `json:"description"`
-	Difficulty  string          `json:"difficulty"`
-	Version     string          `json:"version"`
-	Tags        []string        `json:"tags"`
-	Ports       []docker.Port   `json:"ports"`
-	Volumes     []docker.Volume `json:"volumes"`
-	Privileged  bool            `json:"privileged"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Difficulty  string   `json:"difficulty"`
+	Version     string   `json:"version"`
+	Tags        []string `json:"tags"`
+	Ports       []Port   `json:"ports"`
+	Volumes     []Volume `json:"volumes"`
+	Privileged  bool     `json:"privileged"`
 }
