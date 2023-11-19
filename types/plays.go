bd16d718c79c53836d5923b3b0e1faec25846499
Henry Whitaker <henrywhitaker3@outlook.com>
2023-11-19 20:23:40 +0000

    added play type

--- types/plays.go
+++ types/plays.go
@@ -8,4 +8,12 @@ const (
 	PlayFailed    PlayStatus = "FAILED"
 )
 
-type Play struct{}
+type Play struct {
+	ID        string     `json:"id"`
+	UserID    string     `json:"user_id"`
+	Scenario  string     `json:"scenario"`
+	Driver    string     `json:"driver"`
+	Status    PlayStatus `json:"status"`
+	CreatedAt int64      `json:"created_at"`
+	UpdatedAt int64      `json:"updated_at"`
+}
