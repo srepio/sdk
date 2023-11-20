f88a579e5e9314135c591909e3e32763c9a77450
Henry Whitaker <henrywhitaker3@outlook.com>
2023-11-20 17:39:33 +0000

    add finished at field to play type

--- types/plays.go
+++ types/plays.go
@@ -9,11 +9,12 @@ const (
 )
 
 type Play struct {
-	ID        string     `json:"id"`
-	UserID    string     `json:"user_id"`
-	Scenario  string     `json:"scenario"`
-	Driver    string     `json:"driver"`
-	Status    PlayStatus `json:"status"`
-	CreatedAt int64      `json:"created_at"`
-	UpdatedAt int64      `json:"updated_at"`
+	ID         string     `json:"id"`
+	UserID     string     `json:"user_id"`
+	Scenario   string     `json:"scenario"`
+	Driver     string     `json:"driver"`
+	Status     PlayStatus `json:"status"`
+	CreatedAt  int64      `json:"created_at"`
+	FinishedAt *int64     `json:"finished_at,omitempty"`
+	UpdatedAt  int64      `json:"updated_at"`
 }
