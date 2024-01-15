8eac730de8e2d6d7d950825e802f0c8873bd9c94
Henry Whitaker <henrywhitaker3@outlook.com>
2024-01-15 21:42:32 +0000

    added per page to paginated

--- types/pagintated.go
+++ types/pagintated.go
@@ -4,14 +4,16 @@ type Paginated[T any] struct {
 	Count      int `json:"total"`
 	Page       int `json:"current_page"`
 	TotalPages int `json:"total_pages"`
+	PerPage    int `json:"per_page"`
 	Data       []T `json:"data"`
 }
 
-func NewPaginated[T any](count, page, totalPages int, data []T) *Paginated[T] {
+func NewPaginated[T any](count, page, totalPages, perPage int, data []T) *Paginated[T] {
 	return &Paginated[T]{
 		Count:      count,
 		Page:       page,
 		TotalPages: totalPages,
+		PerPage:    perPage,
 		Data:       data,
 	}
 }
