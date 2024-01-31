2da993aeeb1e7dce069e679e1b9a9d3f677c2f87
Henry Whitaker <henrywhitaker3@outlook.com>
2024-01-31 17:55:51 +0000

    fix: give active play it's own socket event

--- types/pty.go
+++ types/pty.go
@@ -10,6 +10,8 @@ const (
 	// Should be sent in the string format <rows>,<cols>
 	Resize MessgaeType = "resize"
 
+	// When a new active play is sent down the socket
+	ActivePlay MessgaeType = "active_play"
 	// When the play has been checked/cancelled/otherwise killed
 	PlayFinished MessgaeType = "play_finished"
 )
